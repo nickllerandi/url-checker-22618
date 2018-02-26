72a8accf3e619dab9b6508cf8863f962f899c247
Nicholas Llerandi <nicholasllerandi@Nicholass-MacBook-Air.local>
2018-02-26 12:19:32 -0500

    1219

--- src/app.py
+++ src/app.py
@@ -41,5 +41,5 @@ def fiosUpload():
     return str(split_data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(debug=True, port=port)
